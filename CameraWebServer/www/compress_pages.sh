806e286df8b0cc302af8c069ea414aab1f034bed
7FM <>
2020-04-02 18:51:38 +0200

    remove comments before minifying

--- CameraWebServer/www/compress_pages.sh
+++ CameraWebServer/www/compress_pages.sh
@@ -2,6 +2,8 @@
 for file in `ls *.html`; do
     echo "Minify: $file" && \
     cp "$file" "copy_$file" && \
+    sed -i 's:^//.*$::g' "$file" && \
+    sed -i -r 's:\s+//.*$::g' "$file" && \
     sed -i -e 's/  //g' "$file" && \
     sh -c "tr --delete '\n' < \"$file\" > mini_\"$file\"" && \
     sh -c "tr --delete '\r' < mini_\"$file\" > \"$file\"" && \
