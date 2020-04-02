1507e8da03738f8cbf8a63ab587d7884fc0ee5ed
7FM <>
2020-04-02 03:35:24 +0200

    minifiy files before compressing to save some more bytes

--- CameraWebServer/www/compress_pages.sh
+++ CameraWebServer/www/compress_pages.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 for file in `ls *.html`; do
-    echo "Compressing: $file"
+    echo "Minify: $file" && \
     cp "$file" "copy_$file" && \
+    sed -i -e 's/  //g' "$file" && \
+    sh -c "tr --delete '\n' < \"$file\" > mini_\"$file\"" && \
+    sh -c "tr --delete '\r' < mini_\"$file\" > \"$file\"" && \
+    rm mini_"$file" && \
+    echo "Compressing: $file" && \
     gzip -f "$file" && \
     mv "copy_$file" "$file"
 done
